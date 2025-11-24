3164e3f861d933d6876135dfa2ff4a6ac469d8d6
asagaqita <waspoong@yahoo.co.jp>
2025-11-24 14:04:22 +0900

    ログイン後のユーザボタン移動

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,12 @@ import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@cl
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] relative">
+      <div className="absolute top-4 left-4">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-bold">Hello World</h1>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
@@ -18,9 +23,6 @@ export default function Home() {
               </button>
             </SignUpButton>
           </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
         </div>
       </main>
     </div>
